8a9100c810ef9e2a9b010e04ce3b7dc450536e2f
Yoann Dufresne <yoann.dufresne0@gmail.com>
2020-11-25 16:55:26 +0100

    outstr ok

--- encoding.hpp
+++ encoding.hpp
@@ -1,5 +1,6 @@
 #include <cstdint>
 #include <iostream>
+#include <string>
 
 
 /** 
@@ -29,4 +30,31 @@ public:
 	  * @param byte_length Length in Bytes of the sequence.
 	  **/
 	void translate(uint8_t * sequence, size_t byte_length);
+};
+
+
+/** 
+	* Object used to convert compacted sequences from 2 bits per nucleotide to string.
+	* A translation Byte size lookup table is computed during the object creation
+	* and used to fast transform sequences Byte per Byte.
+	**/
+class Stringifyer {
+private:
+	std::string lookup[256];
+
+public:
+	/**
+		* Constructor that construct a 256 Bytes lookup table for fast conversion
+		*
+		* @param source The sequence encoding
+		**/
+	Stringifyer(uint8_t encoding[4]);
+	/**
+	  * read a 2-bits/nucl sequence and return a coresponding string
+	  *
+	  * @param sequence 2-bit compacted sequence that will be converted regarding
+	  * the encodings.
+	  * @param nucl_length Length in nucleotides of the sequence.
+	  **/
+	std::string translate(uint8_t * sequence, size_t nucl_length);
 };
---
+++ outstr.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <string>
+
+#include "outstr.hpp"
+#include "encoding.hpp"
+
+
+using namespace std;
+
+
+Outstr::Outstr() {
+	input_filename = "";
+}
+
+void Outstr::cli_prepare(CLI::App * app) {
+	this->subapp = app->add_subcommand("outstr", "Output kmer sequences as string on stdout.");
+	CLI::Option * input_option = subapp->add_option("-i, --infile", input_filename, "The file to print");
+	input_option->required();
+}
+
+void Outstr::exec() {
+	// Read the encoding and prepare the translator
+	Kff_reader reader = Kff_reader(input_filename);
+	Stringifyer strif(reader.file->encoding);
+
+	// Prepare sequence and data buffers
+	uint8_t * nucleotides;
+	uint8_t * data;
+
+	while (reader.has_next()) {
+		reader.next_kmer(&nucleotides, &data);
+		cout << strif.translate(nucleotides, reader.file->global_vars["k"]) << " " << (uint)*data << endl;
+	}
+}
