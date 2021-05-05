703fa9d8a5b3e3d475562a1eb4dfdabd9eaff846
Yoann Dufresne <yoann.dufresne0@gmail.com>
2021-05-05 14:59:26 +0200

    minimizer function ok for bucket program

---
+++ bucket.hpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+#include "CLI11.hpp"
+#include "kfftools.hpp"
+
+
+#ifndef BUCKET_H
+#define BUCKET_H
+
+class Bucket: public KffTool {
+private:
+	std::string input_filename;
+	std::string output_filename;
+
+  uint8_t * kmer_buffer;
+	uint8_t * data_buffer;
+	uint8_t * copy_buffer;
+
+	uint complement[4];
+
+public:
+	uint m;
+	bool singleside;
+
+	Bucket(uint8_t m=2, bool revcomp=true);
+	~Bucket();
+	void cli_prepare(CLI::App * subapp);
+	void exec();
+
+	void bucketize(uint8_t * kmer, uint8_t * data, uint64_t k);
+	uint get_minimizer(const uint8_t * kmer, const uint64_t k, int & position) const;
+};
+
+#endif
--- kfftools.cpp
+++ kfftools.cpp
@@ -12,7 +12,8 @@
 #include "disjoin.hpp"
 #include "validate.hpp"
 #include "instr.hpp"
-#include "compact.hpp"
+// #include "compact.hpp"
+#include "bucket.hpp"
 
 
 using namespace std;
@@ -71,7 +72,7 @@ int main(int argc, char** argv) {
 	tools.push_back(new Disjoin());
 	tools.push_back(new Validate());
 	tools.push_back(new Instr());
-	tools.push_back(new Compact());
+	tools.push_back(new Bucket());
 
 	// Get the one selected
 	KffTool * tool = parse_args(argc, argv, tools);
