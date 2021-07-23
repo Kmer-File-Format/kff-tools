a1b5d2ea117375bfe5e99d7ec321e50d6b3913b4
Yoann Dufresne <yoann.dufresne0@gmail.com>
2021-07-23 18:28:22 +0200

    test updates for split merge

--- tests/test_tools_commands.py
+++ tests/test_tools_commands.py
@@ -9,7 +9,7 @@ file = "ecoli_count_dsk"
 
 
 class TestInOut(unittest.TestCase):
-  def test_raw_section(self):
+  def test_raw_sections(self):
     print("\n-- TestInOut test_raw_section")
     for k in range(12, 16):
         # Generate random kmer
@@ -61,7 +61,66 @@ class TestInOut(unittest.TestCase):
 
 
 
-# class TestSplitMerge(unittest.TestCase):
+class TestSplitMerge(unittest.TestCase):
+
+    def test_merge_split(self):
+        print(f"\n-- TestMergeSplit test_merge_split")
+        print("  init - generate 3 random kmer files")
+        txt_file_1 = f"txt1_test.txt"
+        kff_file_1 = f"txt1_test.kff"
+        txt_file_2 = f"txt2_test.txt"
+        kff_file_2 = f"txt2_test.kff"
+        txt_file_3 = f"txt3_test.txt"
+        kff_file_3 = f"txt3_test.kff"
+        kg.generate_sequences_file(txt_file_1, 100, 32, size_max=42)
+        kg.generate_random_kmers_file(txt_file_2, 100, 17)
+        kg.generate_random_kmers_file(txt_file_3, 100, 33, max_count=511, overlapping=False)
+
+        # Prepare a file with sections
+        print(f"  1/3 Generate kff files from txts.")
+        self.assertEqual(0, os.system(f"./kff-tools instr -i {txt_file_1} -o {kff_file_1} -k 32 -m 11"))
+        # Read 1 kmer per line
+        self.assertEqual(0, os.system(f"./kff-tools instr -i {txt_file_2} -o {kff_file_2} -k 17"))
+        # Read 1 kmer per line with its counts (up to 255)
+        self.assertEqual(0, os.system(f"./kff-tools instr -i {txt_file_3} -o {kff_file_3} -k 33 -c -d 2"))
+
+
+        print(f"  2/3 Merge files and split it again")
+        merged = "merged_raw_test.kff"
+        split_dir = "split_test/"
+        self.assertEqual(0, os.system(f"rm -r {split_dir} ; mkdir {split_dir}"))
+        self.assertEqual(0, os.system(f"./kff-tools merge -i {kff_file_1} {kff_file_2} {kff_file_3} -o {merged}"))
+        print(f"./kff-tools split --infile {merged} --outdir {split_dir}")
+        self.assertEqual(0, os.system(f"./kff-tools split --infile {merged} --outdir {split_dir}"))
+        
+
+        print(f"  3/3 Compare outputs")
+        self.assertEqual(0, os.system(f"./kff-tools outstr -i {kff_file_1} > {split_dir}/{txt_file_1}_original"))
+        self.assertEqual(0, os.system(f"./kff-tools outstr -i {split_dir}/r_0.kff > {split_dir}/{txt_file_1}_split"))
+        stream = os.popen(f"diff {split_dir}/{txt_file_1}_original {split_dir}/{txt_file_1}_split")
+        stream_val = stream.read()
+        stream.close()
+        self.assertEqual(stream_val, "")
+
+        self.assertEqual(0, os.system(f"./kff-tools outstr -i {kff_file_2} > {split_dir}/{txt_file_2}_original"))
+        self.assertEqual(0, os.system(f"./kff-tools outstr -i {split_dir}/r_1.kff > {split_dir}/{txt_file_2}_split"))
+        stream = os.popen(f"diff {split_dir}/{txt_file_2}_original {split_dir}/{txt_file_2}_split")
+        stream_val = stream.read()
+        stream.close()
+        self.assertEqual(stream_val, "")
+
+        self.assertEqual(0, os.system(f"./kff-tools outstr -i {kff_file_3} > {split_dir}/{txt_file_3}_original"))
+        self.assertEqual(0, os.system(f"./kff-tools outstr -i {split_dir}/r_2.kff > {split_dir}/{txt_file_3}_split"))
+        stream = os.popen(f"diff {split_dir}/{txt_file_3}_original {split_dir}/{txt_file_3}_split")
+        stream_val = stream.read()
+        stream.close()
+        self.assertEqual(stream_val, "")
+
+
+        print("  clean the test area")
+        os.system(f"rm -r {split_dir} {txt_file_1} {kff_file_1} {txt_file_2} {kff_file_2} {txt_file_3} {kff_file_3}")
+
+
 #   def test_basic_split(self):
 #     print(f"\n-- TestSplitMerge test_basic_split")
 #     # Prepare a file with sections
